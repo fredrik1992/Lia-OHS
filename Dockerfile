3c4b50cb3f72188351cac4f84f2d4be3cb73c82b
fredrik1992 <klosansa1@.com>
2021-04-30 10:41:39 +0200

    file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM openjdk:11
 ADD target/ohsapp.jar ohsapp.jar
-EXPOSE 8085
 ENTRYPOINT ["java","-jar","ohsapp.jar"]
--- src/main/java/com/example/ohsapp/OhsappApplication.java
+++ src/main/java/com/example/ohsapp/OhsappApplication.java
@@ -2,8 +2,10 @@ package com.example.ohsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+
 public class OhsappApplication {
 
     public static void main(String[] args) {
